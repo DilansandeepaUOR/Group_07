7abd6ec1296351485aec98081e71d40ebc49bb9b
samindikaLakshan <samindikalakshan71@gmail.com>
2025-03-29 01:03:42 +0530

    Routing created

--- Four_Paws_MS/Frontend/src/App.jsx
+++ Four_Paws_MS/Frontend/src/App.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Pages/Home'
 import Footer from './Components/Footer/Footer'
+import Aboutus from './Pages/Aboutus'
+import Contactus from './Pages/Contactus'
+import Ourservices from './Pages/Ourservices'
+import Petshop from './Pages/Petshop'
+import Pharmacy from './Pages/Pharmacy'
+ 
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Home/>
+    <>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Aboutus" element={<Aboutus />} />
+        <Route path="/Contactus" element={<Contactus />} />
+        <Route path="/Ourservices" element={<Ourservices />} />
+        <Route path="/Petshop" element={<Petshop />} />
+        <Route path="/Pharmacy" element={<Pharmacy />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
       <Footer/>
-    </div>
+      </>
+      
+    
   )
 }
 
