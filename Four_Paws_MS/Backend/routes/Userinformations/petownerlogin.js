f924cc976dae59758272bdf816d132721e597643
samindikaLakshan <samindikalakshan71@gmail.com>
2025-04-01 03:46:05 +0530

    register and login compleated

--- Four_Paws_MS/Backend/routes/Userinformations/petownerlogin.js
+++ Four_Paws_MS/Backend/routes/Userinformations/petownerlogin.js
@@ -2,6 +2,7 @@ const express=require('express');
 const router=express.Router();
 const db=require('../../db');
 const bcrypt= require('bcrypt');
+const jwt = require("jsonwebtoken");
 const validLogin=require('../../validations/loginvalidator');
 
 router.use(express.json());
@@ -20,23 +21,23 @@ router.post("/login", validLogin, async (req,res) => {
         const emailquery='SELECT * FROM pet_owner WHERE E_mail = ?';
 
         db.query(emailquery, [email], async (err, result) => {
-            if (result.length === 0) {
-                return res.status(401).json({error: "email or password does not match"});
-            }
-
             if (err) {
                 return res.status(500).json({ error: "Database error" });
             }
 
+            if (result.length === 0) {
+                return res.status(401).json({error: "email or password does not match"});
+            }
+
             const user=result[0];
 
-            const pwmatch= await bcrypt.compare(password,user.passwordassword);
+            const pwmatch= await bcrypt.compare(password,user.Password);
 
             if(!pwmatch) {
                 return res.status(401).json({error: "email or password does not match"});
             }
 
-            const token = jwt.sign({ id: user.id, email: user.email }, "secret", { expiresIn: "3h" });
+            const token = jwt.sign({ id: user.Owner_id, email: user.E_mail }, "secret", { expiresIn: "3h" });
             res.status(200).json({ message: "Login successful", token });
         });
 
