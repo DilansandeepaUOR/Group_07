4ac3a5fe35353bab97994db38947bcf3f968cdd3
samindikaLakshan <samindikalakshan71@gmail.com>
2025-03-31 22:03:57 +0530

    updated

--- Four_Paws_MS/Backend/routes/Userinformations/petownerlogin.js
+++ Four_Paws_MS/Backend/routes/Userinformations/petownerlogin.js
@@ -1,4 +1,15 @@
 const express=require('express');
 const router=express.Router();
 const db=require('../../db');
+const bcrypt= require('bcrypt');
+const validLogin=require('../../validations/loginvalidator');
+
+router.use(express.json());
+router.use(express.urlencoded({extended: true}));
+
+router.use((req,res,next) => {
+    console.log(`${req.method} request for '${req.url}'`);
+    next();
+});
+
 
---
+++ Four_Paws_MS/Backend/validations/loginvalidator.js
@@ -0,0 +1,18 @@
+const {z} =require('zod');
+
+const userLoginValidator=z.object({
+    email: z.string().email('Invalid Email'),
+    password: z.string().min(8).max(30)
+});
+
+const validLogin= (res, req, next) => {
+    const validationresult=userLoginValidator.safeParse(req.body);
+
+    if(!validationresult.success) {
+        return res.status(400).json({error: validationresult.error.errors});
+    }
+
+    next();
+}
+
+module.exports = validLogin;
