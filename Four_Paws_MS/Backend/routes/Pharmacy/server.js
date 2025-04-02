7e6bbfaa2407cc6d1c4669df4da997fa281ca3f6
Dilan Sandeepa <Dilansandeepa2020@gmail.com>
2025-04-02 22:51:37 +0530

    Update server.js

--- Four_Paws_MS/Backend/routes/Pharmacy/server.js
+++ Four_Paws_MS/Backend/routes/Pharmacy/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json() );
 
 // MySQL Connection Pool
 const pool = mysql.createPool({
